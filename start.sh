04bb51043d4dd59b205670439ba31ab04dc57c1b
f1mmng <105948123+f1mmng@users.noreply.github.com>
2025-09-25 22:59:24 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Install the Python dependencies
-pip install -r requirements.txt
+# Explicitly run the Playwright installation
+playwright install
 
-# Update and install Chromium
-apt-get update -y
-apt-get install -y chromium
-
-# Download and install the Chromium browser for Pyppeteer
-python -m pyppeteer install chromium
-
-# Finally, start your application (replace main.py with your filename)
+# Run your Python application
 python main.py

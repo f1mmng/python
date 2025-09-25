c85ff123cd2c3339ba480ed62fe3c8a858e874dc
f1mmng <105948123+f1mmng@users.noreply.github.com>
2025-09-25 22:11:12 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@
 # Install the Python dependencies
 pip install -r requirements.txt
 
+# Update and install Chromium
+apt-get update -y
+apt-get install -y chromium
+
 # Download and install the Chromium browser for Pyppeteer
 python -m pyppeteer install chromium
 

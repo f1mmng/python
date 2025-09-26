20b016b738c7ba09b749c76e6eeb175b41eae861
f1mmng <105948123+f1mmng@users.noreply.github.com>
2025-09-26 20:33:33 +0200

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-set -e
 
-# Download and install Google Chrome
-wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get update
-apt-get install -y ./google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb

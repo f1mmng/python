47c1dfff024b1a2ff95f1bdccc6768442f4be142
f1mmng <105948123+f1mmng@users.noreply.github.com>
2025-09-26 15:41:57 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
 
-# 1. Install all necessary browser binaries
-echo "--- Starting Playwright Browser Installation ---"
-playwright install
-
-# 2. Run the main Python application
-echo "--- Starting Python Main Script ---"
-# Use exec to ensure the container uses the python process as its primary process
-exec python main.py

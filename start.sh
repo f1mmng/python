648dac8d52bc2a3e1478f3920050cbbfa5912c58
f1mmng <105948123+f1mmng@users.noreply.github.com>
2025-09-26 19:47:49 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-echo "🚀 Starting setup..."
-
-# Ensure pip is up-to-date
-python3 -m pip install --upgrade pip
-
-# Install the Playwright library for Python
-echo "📦 Installing Playwright library..."
-pip install playwright
-
-# Download and install the necessary browsers (Chromium, Firefox, WebKit)
-echo "🌐 Installing browsers for Playwright..."
-playwright install
-
-echo "✅ Setup complete! You can now run the Python script."
+echo "Starting Velo Scraper..."
+# Execute the main Python script
+python main.py
